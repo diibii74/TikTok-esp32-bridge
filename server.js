ec912b38a136afb45c88b7ac9b6d7e65215f52e6
diibii74 <diibii74@gmail.com>
2026-08-29 12:27:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express(); const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => { res.send('Server TikTok WebSocket Attivo!'); });
 const server = http.createServer(app); const wss = new WebSocket.Server({ server });
 // Mantiene attiva la connessione WebSocket inviando un ping ogni 20 secondi setInterval(() => { wss.clients.forEach(client => { if (client.readyState === WebSocket.OPEN) { client.ping(); } }); }, 20000);
-const TIKTOK_USERNAME = 'diibii26';
+const TIKTOK_USERNAME = 'manolita772';
 try { const tiktokLiveConnection = new WebcastPushConnection(TIKTOK_USERNAME);
 tiktokLiveConnection.connect().catch(err => { console.log('TikTok non in Live o errore di connessione:', err.message || err); });
 tiktokLiveConnection.on('chat', data => { const payload = JSON.stringify({ user: data.uniqueId, comment: data.comment });
